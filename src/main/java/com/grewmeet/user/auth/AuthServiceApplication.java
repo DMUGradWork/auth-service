871ac0d8bc6d1005fc4fec3d9405bd521e88f731
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-10-15 18:07:07 +0900

    feat: kafka 삭제

--- src/main/java/com/grewmeet/user/auth/AuthServiceApplication.java
+++ src/main/java/com/grewmeet/user/auth/AuthServiceApplication.java
@@ -1,11 +1,8 @@
 package com.grewmeet.user.auth;
 
-import com.grewmeet.user.auth.event.CustomKafkaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(CustomKafkaProperties.class)
 @SpringBootApplication
 public class AuthServiceApplication {
 
